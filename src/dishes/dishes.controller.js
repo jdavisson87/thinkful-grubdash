736cf31ec4b40c01ba5eef83c9b7b442bd0b869a
jdavisson87 <jdavisson87@gmail.com>
2021-08-31 18:11:31 -0500

    created update controller handler

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -4,10 +4,30 @@ const nextId = require('../utils/nextId');
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
 
+const dishIdIsValid = (req, res, next) => {
+  const { dishId } = req.params;
+  const foundDish = dishes.find((dish) => dish.id === dishId);
+  if (foundDish === undefined) {
+    next({
+      status: 404,
+      message: `The dish Id does not exist: ${dishId}`,
+    });
+  }
+  res.locals.foundDish = foundDish;
+  next();
+};
+
 const bodyIsValid = (req, res, next) => {
-  const {
-    data: { name, description, price, image_url },
-  } = req.body;
+  const dish = req.body.data;
+  const { dishId } = req.params;
+  const { name, description, price, image_url } = dish;
+
+  if (dish.id && dish.id !== dishId) {
+    next({
+      status: 400,
+      message: `Dish id does not match route id.  Dish: ${dish.id} Route: ${dishId}`,
+    });
+  }
   if (!name) {
     next({
       status: 400,
@@ -20,32 +40,42 @@ const bodyIsValid = (req, res, next) => {
       message: `A 'description' property is required`,
     });
   }
-  if (!price || price < 0) {
+  if (!price || price < 0 || !Number.isInteger(price)) {
     next({
       status: 400,
-      message: `A 'price' property is required`,
+      message: `A valid 'price' property is required`,
     });
   }
+
   if (!image_url) {
     next({
       status: 400,
       message: `An 'image_url' property is required`,
     });
   }
+
+  let handleId = undefined;
+  handleId =
+    dish.id === undefined && dishId
+      ? { id: dishId }
+      : dishId
+      ? { id: dishId }
+      : { id: nextId() };
+
+  const newDish = {
+    ...dish,
+    ...handleId,
+  };
+  res.locals.newDish = newDish;
   next();
 };
 
+const read = (req, res) => {
+  res.json({ data: res.locals.foundDish });
+};
+
 const create = (req, res) => {
-  const {
-    data: { name, description, price, image_url },
-  } = req.body;
-  const newDish = {
-    id: nextId(),
-    name,
-    description,
-    price,
-    image_url,
-  };
+  const { newDish } = res.locals;
   dishes.push(newDish);
   res.status(201).json({ data: newDish });
 };
@@ -56,7 +86,21 @@ const list = (req, res) => {
   res.json({ data: dishes.filter(byId) });
 };
 
+const update = (req, res) => {
+  const { newDish, foundDish } = res.locals;
+  if (newDish.id !== foundDish.id) {
+    return next({
+      status: 400,
+      message: `You cannot change existing dish id ${foundDish.id} to ${newDish.id}`,
+    });
+  }
+  const updatedEntry = { ...foundDish, ...newDish };
+  res.json({ data: updatedEntry });
+};
+
 module.exports = {
   create: [bodyIsValid, create],
+  read: [dishIdIsValid, read],
+  update: [dishIdIsValid, bodyIsValid, update],
   list,
 };
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const controller = require('./dishes.controller');
 const methodNotAllowed = require('../errors/methodNotAllowed');
 
-router.route('/:dishId').all(methodNotAllowed);
+router
+  .route('/:dishId')
+  .get(controller.read)
+  .put(controller.update)
+  .all(methodNotAllowed);
 
 router
   .route('/')
